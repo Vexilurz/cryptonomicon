d59b470a5a705e6dd4a0d62a6e02432393302d2e
Sergei Potekhin <vexilurz@gmail.com>
2021-09-13 19:19:52 +0300

    Save selected coins to localstorage

--- src/App.vue
+++ src/App.vue
@@ -226,6 +226,16 @@ export default {
     };
   },
 
+  created() {
+    const tickersData = localStorage.getItem("cryptonomicon-list");
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach((ticker) => {
+        this.subscribeToUpdates(ticker.name);
+      });
+    }
+  },
+
   mounted: async function () {
     await this.fetchCoins();
   },
@@ -266,6 +276,23 @@ export default {
       this.add();
     },
 
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=dc5b16db1d772499cc70f00f5aaee937b4a0b03f343222d134ca65bc52f1a70a`
+        );
+        const data = await f.json();
+        this.tickers.find((t) => t.name === tickerName).price =
+          data.USD > 1 ? data.USD?.toFixed(2) : data.USD?.toPrecision(2);
+
+        if (this.sel?.name === tickerName) {
+          this.graph.push(data.USD);
+        }
+      }, 3000);
+      this.ticker = "";
+      this.suggestedCoins = [];
+    },
+
     add() {
       if (this.tickers.find((t) => t.name === this.ticker)) {
         this.tickerAlreadyAdded = true;
@@ -273,24 +300,14 @@ export default {
       }
       this.tickerAlreadyAdded = false;
       const currentTicker = {
-        name: this.ticker,
+        name: this.ticker.toUpperCase(),
         price: "-"
       };
       this.tickers.push(currentTicker);
-      setInterval(async () => {
-        const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=dc5b16db1d772499cc70f00f5aaee937b4a0b03f343222d134ca65bc52f1a70a`
-        );
-        const data = await f.json();
-        this.tickers.find((t) => t.name === currentTicker.name).price =
-          data.USD > 1 ? data.USD?.toFixed(2) : data.USD?.toPrecision(2);
 
-        if (this.sel?.name === currentTicker.name) {
-          this.graph.push(data.USD);
-        }
-      }, 3000);
-      this.ticker = "";
-      this.suggestedCoins = [];
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+
+      this.subscribeToUpdates(currentTicker.name);
     },
 
     handleDelete(tickerToRemove) {
